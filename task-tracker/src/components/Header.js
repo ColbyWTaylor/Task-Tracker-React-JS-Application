30ed03913f4b73572afa07248e7304e42bf199ae
ColbyWTaylor <colbyt.093@gmail.com>
2021-01-31 21:16:31 -0500

    header outline

--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -2,6 +2,13 @@ function Header(props) {
   return (
     <div>
       <h1>{props.title || "Task Tracker"}</h1>
+      <h2>Tasks:</h2>
+      <ul>
+        <li>Study</li>
+        <li>Play</li>
+        <li>Work</li>
+      </ul>
+      <h2>Done:</h2>
     </div>
   );
 }
