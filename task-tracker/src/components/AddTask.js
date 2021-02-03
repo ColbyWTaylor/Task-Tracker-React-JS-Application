87f71edb18d700f612d98c4677e6bc311522e363
ColbyWTaylor <colbyt.093@gmail.com>
2021-02-03 13:32:42 -0500

    form created, not sending data yet

--- task-tracker/src/components/AddTask.js
+++ task-tracker/src/components/AddTask.js
@@ -1,13 +1,43 @@
+import { useState } from "react";
+
 function AddTask() {
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+  const [reminder, setReminder] = useState(false);
+
   return (
-      <form className="add-form">
+    <form className="add-form">
+      <div className="form-control">
+        <label>Task</label>
+        <input
+          type="text"
+          placeholder="Add Task"
+          text="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
+      <div className="form-control">
+        <label>Day {"&"} Time</label>
+        <input
+          type="text"
+          placeholder="Specify When"
+          value={day}
+          onChange={(e) => setDay(e.target.value)}
+        />
+      </div>
+      <div className="form-control form-control-check">
+        <label>Set Reminder</label>
+        <input
+          type="checkbox"
+          placeholder="Add Reminder?"
+          value={reminder}
+          onChange={(e) => setReminder(e.target.value)}
+        />
+      </div>
 
-          <div className="form-control">
-              <label>Task</label>
-              <input type="text" placeholder="Add Task/>
-
-          </div>
-      </form>
+      <input type="submit" value="Save Task" className="btn btn-block" />
+    </form>
   );
 }
 
