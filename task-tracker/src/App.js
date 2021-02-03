065cf7e991d53a8362113b1eea0692362fc06e86
ColbyWTaylor <colbyt.093@gmail.com>
2021-02-03 14:00:16 -0500

    add task functionality

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -25,10 +25,16 @@ function App() {
     );
   };
 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
